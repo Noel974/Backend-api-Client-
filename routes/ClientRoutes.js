07a6f57e49d46735c5a621a5379ff10ce1601515
Antoine Noël <noelantoine974@outlook.fr>
2025-10-25 11:25:38 +0400

    mise a jour routes

--- routes/ClientRoutes.js
+++ routes/ClientRoutes.js
@@ -8,8 +8,8 @@ router.post('/register', clientController.register);
 router.post('/login', clientController.login);
 
 // ✅ Routes protégées via token
-router.get('/', verifyToken, clientController.getClient);           // Récupère le client connecté
-router.put('/', verifyToken, clientController.updateClient);        // Met à jour le client connecté
-router.delete('/', verifyToken, clientController.deleteClient);     // Supprime le client connecté
+router.get('/getclient', verifyToken, clientController.getClient);           // Récupère le client connecté
+router.put('/updateclient', verifyToken, clientController.updateClient);        // Met à jour le client connecté
+router.delete('/deleteclient', verifyToken, clientController.deleteClient);     // Supprime le client connecté
 
 module.exports = router;
